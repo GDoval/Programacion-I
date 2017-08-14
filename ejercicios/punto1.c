682daf3f3b1ed741dffa6266ca83640b1ef6f3d0
GDoval <charly_sxxi@hotmail.com>
2017-08-14 11:57:44 -0300

    Ejercicio 1 bien hecho

--- ejercicios/punto1.c
+++ ejercicios/punto1.c
@@ -3,24 +3,17 @@
 
 int main()
 {
-    int num1;
-    int num2;
-    int num3;
-    int num4;
-    int num5;
+    int num;
     int suma;
     int promedio;
-    printf("Ingrese un numero: ");
-    scanf("%d", &num1);
-    printf("Ingrese otro numero: ");
-    scanf("%d", &num2);
-    printf("Ingrese otro numero: ");
-    scanf("%d", &num3);
-    printf("Ingrese otro numero: ");
-    scanf("%d", &num4);
-    printf("Ingrese otro numero: ");
-    scanf("%d", &num5);
-    suma = num1 + num2 + num3+ num4 + num5;
+    int x;
+    suma = 0;
+    for (x = 1; x < 6; x++)
+    {
+        printf("Ingrese un numero: ");
+        scanf("%d", &num);
+        suma = suma + num;
+    }
     promedio = suma / 5;
     printf("El promedio de los numeros es %d \n ", promedio);
     system("pause");
