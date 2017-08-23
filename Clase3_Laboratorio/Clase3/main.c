1138c35bcd8bb7810dcfb8fb66cae39d89ad888f
GDoval <charly_sxxi@hotmail.com>
2017-08-23 10:08:51 -0300

    Clase3

--- Clase3_Laboratorio/Clase3/main.c
+++ Clase3_Laboratorio/Clase3/main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 int suma(int n1, int n2);
+int pedirnumero();
 int main()
 {
     int num1, num2, rta;
-    printf("Ingrese un numero: ");
-    scanf("%d", &num1);
-    printf("Ingrese otro numero: ");
-    scanf("%d", &num2);
+    num1 = pedirnumero();
+    num2 = pedirnumero();
     rta = suma(num1, num2);
     printf("La suma es: %d ", rta);
     return 0;
@@ -17,4 +16,12 @@ int suma(int n1, int n2) // Esto iria en un archivo aparte, pero por hoy se deja
     int rsp;
     rsp = n1 + n2;
     return rsp;
+}
+
+int pedirnumero()// Esto iria en un archivo aparte, pero por hoy se deja aca.
+{
+    int num;
+    printf("Ingrese un numero: ");
+    scanf("%d", &num);
+    return num;
 }
