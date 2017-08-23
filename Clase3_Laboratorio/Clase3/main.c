31fac14711d2a39aabe9ae3fd251c111f995a5e6
GDoval <charly_sxxi@hotmail.com>
2017-08-23 10:20:26 -0300

    Clase3

--- Clase3_Laboratorio/Clase3/main.c
+++ Clase3_Laboratorio/Clase3/main.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 int suma(int n1, int n2);
+int resta(int n1, int n2);
+int multi(int n1, int n2);
+int division (int n1, int n2);
 int pedirnumero();
 int main()
 {
-    int num1, num2, rta;
+    int num1, num2, s, r, m, d;
     num1 = pedirnumero();
     num2 = pedirnumero();
-    rta = suma(num1, num2);
-    printf("La suma es: %d ", rta);
+    s = suma(num1, num2);
+    r = resta(num1,num2);
+    m = multi(num1, num2);
+    d = division(num1, num2);
+    printf("La suma es: %d ; la multiplicacion es: %d ; la division es: %d ; la resta es: %d  ", s, m, d, r);
     return 0;
 }
+
 int suma(int n1, int n2) // Esto iria en un archivo aparte, pero por hoy se deja aca.
 {
     int rsp;
@@ -24,4 +31,25 @@ int pedirnumero()// Esto iria en un archivo aparte, pero por hoy se deja aca.
     printf("Ingrese un numero: ");
     scanf("%d", &num);
     return num;
+}
+
+int resta(int n1, int n2)
+{
+    int resta;
+    resta = n1 - n2;
+    return resta;
+}
+
+int division(int n1, int n2)
+{
+    int division;
+    division = n1 / n2;
+    return division;
+}
+
+int multi(int n1, int n2)
+{
+    int multi;
+    multi = n1 * n2;
+    return multi;
 }
