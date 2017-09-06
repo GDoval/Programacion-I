bddd6f1478e2c1b78cc075dc006e6b951b602dc6
GDoval <charly_sxxi@hotmail.com>
2017-09-06 10:46:50 -0300

    Ejercicios con strings.
    Signed-off-by: GDoval <charly_sxxi@hotmail.com>

---
+++ Clase_7_Lab/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char nombre[30];
+    char apellido[30];
+    char buffer[2000];
+    char apellidoNombre[60];
+    int cant;
+    printf("Ingrese su nombre: ");
+    fgets(buffer, 2000, stdin);
+    cant = strlen(buffer);
+    while (cant > 28)
+    {
+        printf("Error, reingrese: ");
+        fgets(buffer, 2000, stdin);
+        cant = strlen(buffer);
+    }
+    strcpy(nombre, buffer);
+    nombre[cant - 1] = '\0';
+    printf("Ingrese su apellido: ");
+    fgets(buffer, 2000, stdin);
+    cant = strlen(buffer);
+    while (cant > 28)
+    {
+        printf("Error, reingrese: ");
+        fgets(buffer, 2000, stdin);
+        cant = strlen(buffer);
+    }
+    strcpy(apellido, buffer);
+    apellido[cant - 1] = '\0';
+    printf("%s , %s\n", nombre, apellido);
+    strupr(&nombre[0]);
+    strupr(&apellido[0]);
+    strlwr(&nombre[1]);
+    strlwr(&apellido[1]);
+    printf("%s , %s\n", nombre, apellido);
+    strcat(apellido, ",");
+    strcat(apellido, " ");
+    strcat(apellido, nombre);
+    strcpy(apellidoNombre, apellido);
+    printf("%s", apellidoNombre);
+
+
+    return 0;
+}
