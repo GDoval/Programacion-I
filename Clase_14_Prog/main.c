b2c006ab0e02edec0079c7fc6ccd804f713fb5bd
GDoval <charly_sxxi@hotmail.com>
2017-10-09 10:37:13 -0300

    Ejercicios con punteros. Leer los comentarios
    Signed-off-by: GDoval <charly_sxxi@hotmail.com>

--- Clase_14_Prog/main.c
+++ Clase_14_Prog/main.c
@@ -9,7 +9,7 @@ int main()
     int y = 18;
     int vec[] = {6,7,4,5,9};
     mostrarVector(vec, 5);
-    /*printf("Valores antes del swap\n\n");
+    printf("\n\nValores antes del swap\n\n");
     printf("X = %d\n", x);
     printf("Y = %d\n", y);
 
@@ -17,7 +17,7 @@ int main()
 
     printf("\nValores despues del swap y en el main\n\n");
     printf("X = %d\n", x);
-    printf("Y = %d\n", y);*/
+    printf("Y = %d\n", y);
     return 0;
 }
 
@@ -38,6 +38,6 @@ void mostrarVector (int* x, int tam)
 {
     for (int i = 0; i < tam; i++)
     {
-        printf("%d  ", (*x + i)); // suma a la direccion de memoria guardada en *x el valor de i. Donde i valdria el sizeof() de int.
+        printf("%d  ", *(x + i)); // suma a la direccion de memoria guardada en *x el valor de i. Donde i valdria el sizeof() de int.
     }                             // O sea, muestra el valor de la direccion de memoria 0, despues la 1, despues la 2, etc
 }                                 //Los vectores arrancan en la posicion 0 y se van moviendo la cantidad de bytes que tenga el tipo de dato del que son
