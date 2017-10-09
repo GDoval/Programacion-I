e2d502b3cf622d0f95f07841891fb6f51f5e20a8
GDoval <charly_sxxi@hotmail.com>
2017-10-09 11:36:57 -0300

    Signed-off-by: GDoval <charly_sxxi@hotmail.com>

---
+++ Clase_14_Prog_Estructuras/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct{
+    char modelo[20];
+    int nucleos;
+} eProcesador;
+
+typedef struct {
+
+    char marca [20];
+    eProcesador procesador;
+    int ram;
+    float precio;
+} eNotebook;
+
+
+int main()
+{
+
+    return 0;
+}
