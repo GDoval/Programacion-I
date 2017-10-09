ea4a98f50673080b1cf478066b21c46858ec16b7
GDoval <charly_sxxi@hotmail.com>
2017-10-09 12:16:43 -0300

    Hacer funcion que esta en el cuadernillo

--- Clase_14_Prog_Estructuras/main.c
+++ Clase_14_Prog_Estructuras/main.c
@@ -13,9 +13,42 @@ typedef struct {
     float precio;
 } eNotebook;
 
+void mostrarNotebook (eNotebook);
+void cargarNotebook (eNotebook*);
 
 int main()
 {
-
+    eNotebook n;// = {"Intel",{"I7", 4}, 18, 1000.50};
+    cargarNotebook(&n);
+    mostrarNotebook(n);
     return 0;
+}
+
+
+
+void mostrarNotebook (eNotebook note)
+{
+    printf("\nMarca: %s", note.marca);
+    printf("\nModelo: %s", note.procesador.modelo);
+    printf("\nNucleos: %d", note.procesador.nucleos);
+    printf("\nRAM: %d", note.ram);
+    printf("\nPrecio: %.2f\n", note.precio);
+
+}
+
+
+void cargarNotebook (eNotebook* x)
+{
+    printf("Ingrese marca: ");
+    fflush(stdin);
+    fgets(x->marca, 20, stdin);
+    printf("\nIngrese modelo de procesador: ");
+    fflush(stdin);
+    fgets(x->procesador.modelo, 20, stdin);
+    printf("\nIngrese cantidad de nucleos: ");
+    scanf("%d", &x->procesador.nucleos);
+    printf("\nIngrese cantidad de RAM: ");
+    scanf("%d", &x->ram);
+    printf("\nIngrese precio: ");
+    scanf("%f", &x->precio);
 }
