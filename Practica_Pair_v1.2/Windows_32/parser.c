5e831f62d5fc7b190256ecadbebbb957d562f703
GDoval <charly_sxxi@hotmail.com>
2017-10-30 12:15:04 -0300

    Ejercicios con Arraylist

--- Practica_Pair_v1.2/Windows_32/parser.c
+++ Practica_Pair_v1.2/Windows_32/parser.c
@@ -3,23 +3,46 @@
 #include "ArrayList.h"
 #include "Employee.h"
 
-
+/*Trae los campos que estan en el archivo .csv y los pasa adentro del ArrayList. Recibe un puntero al archivo,
+que se abre fuera de la funcion, y un puntero al ArrayList, que se crea en el main(). Como el scanf() lee solo texto
+se tienen que hacer buffers de tipo char para guardar los campos del archivo. Esos buffer se parsean al tipo de datos
+que corresponde, y esos son los valores que se guardan en el ArrayList.
+*/
 int parserEmployee(FILE* pFile , ArrayList* pArrayListEmployee)
 {
-    Employee* aux;
-    char id[500], name[500], lastName[500], isEmpty[500];
-    fscanf(pFile, "%[^,],%[^,],%[^,],%[^\n]\n", id, name, lastName, isEmpty);
-    int id;
-    id = atoi(id);
-    aux->id = id;
-    strcpy(aux->name, name);
-    strcpy(aux->lastName, lastName);
-    if (strcmp(isEmpty, "true") == 0)
-    {
-        aux->isEmpty = 1;
-    }else
-    {
-        aux->isEmpty = 0;
+    char auxInt[10];
+    char auxNombre[51];
+    char auxApellido[51];
+    char auxEmpty[10];
+    Employee* nuevoEmpleado; // Crea una variable de tipo puntero para guardar despues la direccion de memoria que devuelve la funcion new_Employee()
+
+    if(pFile == NULL || pArrayListEmployee == NULL){ //si no pudo abrir alguno de los punteros sale de la funcion
+            return -1;
     }
+
+    fscanf(pFile, "%[^,],%[^,],%[^,],%s\n", auxInt, auxNombre, auxApellido, auxEmpty); // descarta la primer linea del archivo xq tiene basura
+
+    while(!feof(pFile)){
+         fscanf(pFile, "%[^,],%[^,],%[^,],%s\n", auxInt, auxNombre, auxApellido, auxEmpty);
+
+         nuevoEmpleado = employee_new(); // se guarda espacio en memoria para la estructura que se va a guardar en el ArrayList
+
+         if(nuevoEmpleado !=NULL){                          //Aca se parsean los buffer
+            nuevoEmpleado->id = atoi(auxInt);
+            strcpy(nuevoEmpleado->name, auxNombre);
+            strcpy(nuevoEmpleado->lastName, auxApellido);
+            if(strcmp(auxEmpty, "false")== 0){
+                nuevoEmpleado->isEmpty = 0;
+            }
+            else{
+                nuevoEmpleado->isEmpty = 1;
+            }
+
+            pArrayListEmployee->add(pArrayListEmployee, nuevoEmpleado); // aca se agregan al ArrayList
+
+            }
+
+         }
+
     return 0;
 }
