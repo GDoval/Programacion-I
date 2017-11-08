84ef7662563cd58bcc3698721b51acaafee2678e
GDoval <charly_sxxi@hotmail.com>
2017-11-08 09:12:24 -0300

    Compila las dos bibliotecas del ArrayList. Faltaria de terminar los testing en el main().

---
+++ ArrayList_Windows/estructuras.c
@@ -0,0 +1,82 @@
+#include "estructuras.h"
+#include "ArrayList.h"
+
+
+
+int parseArchivo(FILE* archivo, ArrayList* lista)
+{
+    char auxId[100];
+    char auxNom[100];
+    char auxApe[100];
+    char auxVacio[100];
+    ePersona* nuevaPersona;
+    if (archivo == NULL || lista == NULL)
+        return -1;
+
+    fscanf(archivo, "%[^,],%[^,],%[^,],%s\n", auxId, auxNom, auxApe, auxVacio);
+    while(!feof(archivo))
+    {
+        fscanf(archivo, "%[^,],%[^,],%[^,],%s\n", auxId, auxNom, auxApe, auxVacio);
+        nuevaPersona = persona_constructor();
+        if (nuevaPersona != NULL)
+        {
+            nuevaPersona->id = atoi(auxId);
+            strcpy(nuevaPersona->nombre, auxNom);
+            strcpy(nuevaPersona->apellido, auxApe);
+            if (strcmp(auxVacio, "false") == 0)
+            {
+                nuevaPersona->vacio = 0;
+            }else
+            {
+                nuevaPersona->vacio = 1;
+            }
+        }
+        lista->add(lista, nuevaPersona);
+
+    }
+    return 0;
+}
+
+
+
+ePersona* persona_constructor()
+{
+    ePersona* auxiliar;
+    auxiliar = (ePersona*) malloc(sizeof(ePersona));
+    if (auxiliar != NULL)
+    {
+        auxiliar->id = 0;
+        strcpy(auxiliar->nombre, "");
+        strcpy(auxiliar->apellido, "");
+        auxiliar->vacio = 0;
+    }
+    return auxiliar;
+}
+
+
+void imprimir(ArrayList* lista)
+{
+    ePersona* persona;
+    printf("ID     Nombre     Apellido\n\n ");
+    for (int i = 0; i < lista->size; i++)
+    {
+        persona = (ePersona*)lista->get(lista, i);
+        printf("%d    %s          %s\n", persona->id, persona->nombre, persona->apellido);
+    }
+}
+
+int compararNombre (void* persona1, void* persona2)
+{
+    if (strcmp(((ePersona*)persona1)->nombre, ((ePersona*)persona2)->nombre) > 0)
+        return 1;
+    return 0;
+}
+
+
+
+void sacarEnter(char vec[]) // borra el enter que queda en la ultima posicion del string cuando se usa fgets().
+{
+    int cant;
+    cant = strlen(vec);
+    vec[cant-1] = '\0';
+}
---
+++ ArrayList_Windows/main.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ArrayList.h"
+#include "estructuras.h"
+
+int main()
+{
+    FILE* miArchivo;
+    ArrayList* lista = al_newArrayList();
+    ArrayList* clonada;
+    ArrayList* sublista;
+    ePersona* persona;
+    ePersona extra;
+    persona = &extra;
+    int r, f;
+    int opcion;
+    char resp = 's';
+    while (resp == 's')
+    {
+        printf("1) Parse\n");
+        printf("2) Imprimir listado\n");
+        printf("3) Ordenar por nombre\n");
+        printf("4) Clonar lista\n");
+        printf("5) Ordenar lista clonada\n");
+        printf("6) Imprimir lista origen y lista clonada\n");
+        printf("7) Eliminar persona\n");
+        printf("8) Agregar persona\n");
+        printf("9) Imprimir desde hasta\n");
+        printf("10) Size lista origen\n");
+        printf("11) Size lista clonada\n");
+        printf("12) Contains en lista clonada lo de la lista origen\n");
+        printf("13) cls lista origen\n");
+        printf("14) Indice del elemento\n");
+        printf("15) Comprar lista clon y origen a ver si son iguales\n");
+        scanf("%d", &opcion);
+        switch (opcion)
+        {
+        case 1:
+            miArchivo = fopen("data.csv", "r");
+            parseArchivo(miArchivo, lista);
+            break;
+        case 2:
+            system("cls");
+            imprimir(lista);
+            break;
+        case 3:
+            lista->sort(lista, compararNombre, 1);
+            break;
+        case 4:
+            clonada = lista->clone(lista);
+            break;
+        case 5:
+            clonada->sort(clonada, compararNombre, 1);
+            break;
+        case 6:
+            system("cls");
+            imprimir(lista);
+            printf("\n-----------------------------------------------\n");
+            imprimir(clonada);
+            break;
+        case 7:
+            system("cls");
+            printf("Ingrese ID a eliminar: ");
+            scanf("%d", &r);
+            lista->remove(lista, r);
+            break;
+        case 8:
+            system("cls");
+            printf("Ingrese ID: ");
+            scanf("%d", &persona->id);
+            printf("\nIngrese nombre: ");
+            setbuf(stdin, NULL);
+            fgets(persona->nombre, 100, stdin);
+            sacarEnter(persona->nombre);
+            printf("\nIngrese apellido: ");
+            setbuf(stdin, NULL);
+            fgets(persona->apellido, 100, stdin);
+            sacarEnter(persona->apellido);
+            lista->add(lista, persona);
+            break;
+        case 9:
+            system("cls");
+            printf("Ingrese indice desde: ");
+            scanf("%d", &r);
+            printf("\nIngrese indice hasta: ");
+            scanf("%d", &f);
+            sublista = lista->subList(lista, r, f);
+            imprimir(sublista);
+            break;
+        case 10:
+            system("cls");
+            printf("Tamanio lista origen: %d\n\n", lista->size);
+            break;
+        case 11:
+            system("cls");
+            printf("Tamanio lista clonada: %d\n\n", clonada->size);
+            break;
+        case 12:
+            system("cls");
+            r = lista->containsAll(lista, clonada);
+            if (r == 1)
+            {
+                printf("Listas iguales RAAAAAAAAAAUUUUUUUUUULLLLLLLLLLL\n\n");
+            }else
+            {
+                printf("Son distintas !!!!!!!!!!!!!!! \n\n");
+            }
+            break;
+        case 13:
+            system("cls");
+
+        }
+    }
+    return 0;
+}
