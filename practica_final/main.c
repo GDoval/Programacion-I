39c507c1bb8ffd1f4daecde171c07d49ba5e4a56
GDoval <soporte@imsp.intermedia>
2017-12-06 16:22:09 -0300

    Listos los parse

--- practica_final/main.c
+++ practica_final/main.c
@@ -2,12 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include "ArrayList.h"
+#include "parse.h"
 
 int main()
 {
-    FILE* archivo;
+    //FILE* archivo;
     ArrayList* listaArray = al_newArrayList();
-    parse_Log(listaArray);
-    imprimir(listaArray);
+    ArrayList* arrayService = al_newArrayList();
+    /*parse_Log(listaArray);
+    imprimir(listaArray);*/
+    /*FILE* archivo;
+    char auxId[100];
+    char auxEmail[100];
+    char auxName[100];
+    S_Service* auxiliar;
+    S_Service* uno = constructor_service();
+    archivo = fopen("service.txt", "r");
+    fscanf(archivo, "%[^;];%[^;];%[^\n]\n", auxId, auxEmail, auxName);
+    strcpy(uno->name, auxName);
+    printf("%s", uno->name);*/
+    parse_sistema(arrayService);
+    imprimir_service(arrayService);
     return 0;
 }
--- practica_final/parse.c
+++ practica_final/parse.c
@@ -10,13 +10,13 @@ int parse_Log(ArrayList* lista)
     char AuxServiceId[100];
     char AuxGravedad[100];
     char AuxMsg[650];
-    archivo = fopen("log.txt", "r");
+    archivo = fopen("log.txt", "r");// falta validar que no sea NULL
     S_LogEntry* nuevoLog;
     while(!feof(archivo))
     {   //Funciona bien, la ultima mascara lee todo hasta el salto de pagina
         fscanf(archivo, "%[^;];%[^;];%[^;];%[^;];%[^\n]\n", auxDate, auxTime, AuxServiceId, AuxGravedad, AuxMsg);
         nuevoLog = log_constructor();
-        if (nuevoLog != NULL)
+        if (nuevoLog != NULL) //faltaria usar los setters
         {
             strcpy(nuevoLog->date, auxDate);
             strcpy(nuevoLog->time, auxTime);
@@ -46,6 +46,44 @@ S_LogEntry* log_constructor()
 }
 
 
+S_Service* constructor_service()
+{
+    S_Service* aux;
+    aux = (S_Service*) malloc(sizeof(S_Service)); //el sizeof es del tamaño de la estructura, no del puntero a estructura
+    if (aux != NULL)                              //Lo que se castea es lo que devuelve malloc()
+    {
+        aux->id = 0;
+        strcpy(aux->email, "");
+        strcpy(aux->name, "");
+    }
+    return aux;
+}
+
+
+int parse_sistema(ArrayList* lista)
+{
+    FILE* archivo;
+    char auxId[100];
+    char auxEmail[100];
+    char auxName[100];
+    S_Service* auxiliar;
+    archivo = fopen("service.txt", "r"); // falta validar que no sea NULL archivo
+    while (!feof(archivo))
+    {
+        fscanf(archivo,"%[^;];%[^;];%[^\n]\n", auxId, auxEmail, auxName);
+        auxiliar =constructor_service();
+        if (auxiliar != NULL)
+        {
+            auxiliar->id = atoi(auxId);
+            set_Name(auxiliar, auxName);
+            set_Mail(auxiliar, auxEmail);
+        }
+        lista->add(lista, auxiliar);
+    }
+    fclose(archivo);
+}
+
+
 void imprimir(ArrayList* lista)
 {
     S_LogEntry* log;
@@ -55,4 +93,27 @@ void imprimir(ArrayList* lista)
         log = lista->get(lista, i);
         printf("%s   %s   %d   %d   %s \n", log->date, log->time, log->serviceId, log->gravedad, log->msg);
     }
+}
+
+
+void imprimir_service(ArrayList* lista)
+{
+    S_Service* service;
+    int i;
+    for (i = 0; i < lista->len(lista); i++)
+    {
+        service = lista->get(lista, i);
+        printf("%d     %s     %s\n", service->id, service->email, service->name);
+    }
+}
+
+void set_Name(S_Service* persona, char* nombre)
+{
+    strcpy(persona->name, nombre);
+}
+
+
+void set_Mail(S_Service* persona, char* mail)
+{
+    strcpy(persona->email, mail);
 }
